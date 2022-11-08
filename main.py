3602741aab98a49c0beaed694ca13e7a898355a1
Eric Salazar <67451306+DieRandomDie@users.noreply.github.com>
2022-11-08 09:53:40 -0600

    added tkinter remaining time display

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import time
-import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime, sleep, time
 from threading import Timer
 from pyautogui import press, typewrite
 
-next_hunt_time = 0
-next_work_time = 0
-next_farm_time = 0
-huntcmd = "rpg hunt t"
+next_hunt_time = 40
+next_work_time = 196
+next_farm_time = 391
+
 huntcd = 40
+workcd = 196
+farmcd = 391
+
+huntcmd = "rpg hunt t"
 
 area = int(input("Area?\n> "))
 if area < 3:
@@ -21,16 +26,48 @@ elif area == 8:
 elif area >= 9:
     workcmd = "rpg chainsaw"
 
-workcd = 196
 farmcmd = "rpg farm"
-farmcd = 391
 
 
+def time_check(cmdtype):
+    global next_hunt_time
+    global next_work_time
+    global next_farm_time
+
+    total = 0
+
+    if cmdtype == "hunt":
+        if abs(time()+huntcd-next_work_time) < 1.2:
+            total = total + 1.2
+            print("hunt +1")
+        if abs(time()+huntcd-next_farm_time) < 1.2:
+            total = total + 1.2
+            print("hunt +1")
+    elif cmdtype == "work":
+        if abs(time()+workcd-next_hunt_time) < 1.2:
+            total = total + 1.2
+            print("work +1")
+        if abs(time()+workcd-next_farm_time) < 1.2:
+            total = total + 1.2
+            print("work +1")
+    elif cmdtype == "farm":
+        if abs(time()+farmcd-next_hunt_time) < 1.2:
+            total = total + 1.2
+            print("farm +1")
+        if abs(time()+farmcd-next_work_time) < 1.2:
+            total = total + 1.2
+            print("farm +1")
+    print(cmdtype, ": ", total)
+    return total
+
+
+# commands
 def hunt(cmd, cd):
     typewrite(cmd)
     press('enter')
     global next_hunt_time
-    next_hunt_time = time.time()+cd
+    cd = cd + time_check("hunt")
+    next_hunt_time = time() + cd
     hunt_timer = Timer(cd, hunt, [huntcmd, huntcd])
     hunt_timer.start()
 
@@ -39,7 +76,8 @@ def work(cmd, cd):
     typewrite(cmd)
     press('enter')
     global next_work_time
-    next_work_time = time.time()+cd
+    cd = cd + time_check("work")
+    next_work_time = time() + cd
     work_timer = Timer(cd, work, [workcmd, workcd])
     work_timer.start()
 
@@ -48,28 +86,61 @@ def farm(cmd, cd):
     typewrite(cmd)
     press('enter')
     global next_farm_time
-    next_farm_time = time.time()+cd
+    cd = cd + time_check("farm")
+    next_farm_time = time() + cd
     farm_timer = Timer(cd, farm, [farmcmd, farmcd])
     farm_timer.start()
 
 
-window = tk.Tk()
-time.sleep(3)
+root = Tk()
+root.title('Discord Bot Botter')
+
+
+hunt_label = Label(root, font=('calibri', 40, 'bold'),background='black',foreground='white')
+work_label = Label(root, font=('calibri', 40, 'bold'),background='black',foreground='white')
+farm_label = Label(root, font=('calibri', 40, 'bold'),background='black',foreground='white')
+
+# Placing clock at the centre
+# of the tkinter window
+hunt_label.pack(anchor='center')
+work_label.pack(anchor='center')
+farm_label.pack(anchor='center')
+
+
+sleep(3)
 hunt(huntcmd, huntcd)
-time.sleep(1.2)
+sleep(1.2)
 work(workcmd, workcd)
-time.sleep(1.2)
-if area >= 3:
+sleep(1.2)
+if area >= 4:
     farm(farmcmd, farmcd)
 
 
-#while True:
-#    hunt_time_remaining = '%.2f'%(next_hunt_time - time.time())
-#    work_time_remaining = '%.2f'%(next_work_time - time.time())
-#    if area >= 3:
-#        farm_time_remaining = '%.2f'%(next_farm_time - time.time())
-#    else
-#        farm_time_remaining = "Not Farming"
-#    time.sleep(.05)
+def update_hunt():
+    hunt_string = '%.1f'%(next_hunt_time - time())
+    hunt_label.config(text="Hunt: "+hunt_string)
+    hunt_label.after(90, update_hunt)
 
 
+def update_work():
+    work_string = '%.1f'%(next_work_time - time())
+    work_label.config(text="Work: "+work_string)
+    work_label.after(90, update_work)
+
+
+def update_farm():
+    if area >= 4:
+        farm_string = '%.1f'%(next_farm_time - time())
+    else:
+        farm_string = "Not Farming"
+    farm_label.config(text="Farm: "+farm_string)
+    farm_label.after(90, update_farm)
+
+
+hunt_label.pack(anchor='center')
+work_label.pack(anchor='center')
+farm_label.pack(anchor='center')
+update_hunt()
+update_work()
+update_farm()
+mainloop()
